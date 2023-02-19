9af30890bc211a610d9990d64be945b4f0eef1e5
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-02-19 22:26:10 +0530

    Added dynamic detection of sink in farm house Replace the name of kitchen with stove

--- stardew-access/Features/TileInfo.cs
+++ stardew-access/Features/TileInfo.cs
@@ -455,15 +455,19 @@ namespace stardew_access.Features
             else if (Game1.currentLocation is FarmHouse farmHouse)
             {
                 if (farmHouse.upgradeLevel >= 1)
-                    if (farmHouse.getKitchenStandingSpot().X == x && (farmHouse.getKitchenStandingSpot().Y - 1) == y) // Standing spot is where the player will stand
-                        return (CATEGORY.Interactables, "Kitchen");
+                    if (farmHouse.getKitchenStandingSpot().X == x && (farmHouse.getKitchenStandingSpot().Y - 1) == y)
+                        return (CATEGORY.Interactables, "Stove");
+                    else if ((farmHouse.getKitchenStandingSpot().X + 1) == x && (farmHouse.getKitchenStandingSpot().Y - 1) == y)
+                        return (CATEGORY.Others, "Sink");
                     else if (farmHouse.fridgePosition.X == x && farmHouse.fridgePosition.Y == y)
                         return (CATEGORY.Interactables, "Fridge");
             }
             else if (Game1.currentLocation is IslandFarmHouse islandFarmHouse)
             {
-                if ((islandFarmHouse.fridgePosition.X - 1) == x && islandFarmHouse.fridgePosition.Y == y)
-                    return (CATEGORY.Interactables, "Kitchen");
+                if ((islandFarmHouse.fridgePosition.X - 2) == x && islandFarmHouse.fridgePosition.Y == y)
+                    return (CATEGORY.Interactables, "Stove");
+                else if ((islandFarmHouse.fridgePosition.X - 1) == x && islandFarmHouse.fridgePosition.Y == y)
+                    return (CATEGORY.Others, "Sink");
                 else if (islandFarmHouse.fridgePosition.X == x && islandFarmHouse.fridgePosition.Y == y)
                     return (CATEGORY.Interactables, "Fridge");
             }
