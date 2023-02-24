15380e5b764f763eeed4d1468b049faccc1c97ec
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-02-24 20:44:54 +0530

    Improved game menu patch code - Added active suffix if the hovered tab button matches the current tab

--- stardew-access/Patches/GameMenuPatches/GameMenuPatch.cs
+++ stardew-access/Patches/GameMenuPatches/GameMenuPatch.cs
@@ -11,26 +11,28 @@ namespace stardew_access.Patches
         {
             try
             {
-                // Continue if only in the Inventory Page or Crafting Page
-                if (__instance.currentTab != 0 && __instance.currentTab != 4 && __instance.currentTab != 6 && __instance.currentTab != 7)
+                // Skip if in map page
+                if (__instance.currentTab == 3)
                     return;
 
                 int x = Game1.getMouseX(true), y = Game1.getMouseY(true); // Mouse x and y position
 
                 for (int i = 0; i < __instance.tabs.Count; i++)
                 {
-                    if (__instance.tabs[i].containsPoint(x, y))
+                    if (!__instance.tabs[i].containsPoint(x, y))
+                        continue;
+
+                    string toSpeak = $"{GameMenu.getLabelOfTabFromIndex(i)} Tab" + ((i == __instance.currentTab) ? " Active" : "");
+                    if (gameMenuQueryKey != toSpeak)
                     {
-                        string toSpeak = $"{GameMenu.getLabelOfTabFromIndex(i)} Tab";
-                        if (gameMenuQueryKey != toSpeak)
-                        {
-                            MainClass.DebugLog("here");
-                            gameMenuQueryKey = toSpeak;
-                            MainClass.ScreenReader.Say(toSpeak, true);
-                        }
-                        return;
+                        gameMenuQueryKey = toSpeak;
+                        MainClass.ScreenReader.Say(toSpeak, true);
                     }
+                    return;
                 }
+
+                // If not hovering on any tab button
+                Cleanup();
             }
             catch (Exception e)
             {
