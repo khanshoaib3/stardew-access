5f612a20c134356a6a4be92b0eeff7d395618fae
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-04-10 20:04:58 +0530

    Patched the checkAction method instead to avoid the patch from executing multiple times

--- stardew-access/Patches/MiscPatches/TrashBearPatch.cs
+++ stardew-access/Patches/MiscPatches/TrashBearPatch.cs
@@ -1,22 +1,23 @@
 using StardewValley;
+using StardewValley.Characters;
 
 namespace stardew_access.Patches
 {
     internal class TrashBearPatch
     {
-        internal static void DrawPatch(int ___itemWantedIndex, int ___showWantBubbleTimer)
+        internal static void CheckActionPatch(TrashBear __instance, bool __result, int ___itemWantedIndex, int ___showWantBubbleTimer)
         {
             try
             {
-                if (___showWantBubbleTimer >= 2900)
-                {
-                    string itemName = Game1.objectInformation[___itemWantedIndex].Split('/')[4];
-                    MainClass.ScreenReader.Say($"Trash Bear wants {itemName}!", true);
-                }
+                if (__result) return; // The true `true` value of __result indicates the bear is interactable i.e. when giving the bear the wanted item
+                if (__instance.sprite.Value.CurrentAnimation != null) return;
+
+                string itemName = Game1.objectInformation[___itemWantedIndex].Split('/')[4];
+                MainClass.ScreenReader.Say($"Trash Bear wants {itemName}!", true);
             }
             catch (Exception e)
             {
-                MainClass.ErrorLog($"An error occured TrashBearPatch::DrawPatch():\n{e.Message}\n{e.StackTrace}");
+                MainClass.ErrorLog($"An error occured TrashBearPatch::CheckActionPatch():\n{e.Message}\n{e.StackTrace}");
             }
         }
     }
