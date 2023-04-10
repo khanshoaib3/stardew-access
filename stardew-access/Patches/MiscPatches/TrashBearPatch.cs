c6bef6111886421784e97a24b396c85b2c1f58e5
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-04-10 22:32:52 +0530

    Added translations for trash bear wanted item Added missing translations for heart count entry

--- stardew-access/Patches/MiscPatches/TrashBearPatch.cs
+++ stardew-access/Patches/MiscPatches/TrashBearPatch.cs
@@ -13,7 +13,7 @@ namespace stardew_access.Patches
                 if (__instance.sprite.Value.CurrentAnimation != null) return;
 
                 string itemName = Game1.objectInformation[___itemWantedIndex].Split('/')[4];
-                MainClass.ScreenReader.Say($"Trash Bear wants {itemName}!", true);
+                MainClass.ScreenReader.Say(MainClass.Translate("patch.trash_bear.wanted_item", new {trash_bear_name = __instance.displayName, item_name = itemName}), true);
             }
             catch (Exception e)
             {
