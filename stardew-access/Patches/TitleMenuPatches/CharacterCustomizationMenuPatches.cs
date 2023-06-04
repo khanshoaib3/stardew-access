ff6ef50cc1a00f7489638a8589c1197994c5a8f0
Katie Durden <1766632+ParadoxiKat@users.noreply.github.com>
2023-06-04 14:43:49 -0700

    Move ColorMatcher class to it's own file in new namespace `stardew_access.Utils` (advances khanshoaib3/stardew-access#112).

--- stardew-access/Patches/TitleMenuPatches/CharacterCustomizationMenuPatches.cs
+++ stardew-access/Patches/TitleMenuPatches/CharacterCustomizationMenuPatches.cs
@@ -2,7 +2,7 @@ using System.Text.Json;
 using StardewValley;
 using StardewValley.Menus;
 using static stardew_access.Features.Utils;
-using static stardew_access.Features.ColorMatcher;
+using static stardew_access.Utils.ColorMatcher;
 
 namespace stardew_access.Patches
 {
