98d039c16626473b3e64533224e2a8963e1cba47
Katie Durden <1766632+ParadoxiKat@users.noreply.github.com>
2023-06-04 17:58:43 -0700

    Split remaining class and enum in Utils.cs into Category.cs and MachineState.cs (khanshoaib3/stardew-access#112)

---
+++ stardew-access/Utils/MachineState.cs
@@ -0,0 +1,11 @@
+//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+
+namespace stardew_access.Utils
+{
+    public enum MachineState
+    {
+        Ready, Busy, Waiting
+    }
+}
