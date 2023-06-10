ce9525cde384f4a24fe90fc9a3f81b5be84efbdd
Katie Durden <1766632+ParadoxiKat@users.noreply.github.com>
2023-06-10 16:12:10 -0700

    Fix CA2101	Specify marshaling for P/Invoke string arguments

--- stardew-access/ScreenReader/ScreenReaderMac.cs
+++ stardew-access/ScreenReader/ScreenReaderMac.cs
@@ -26,7 +26,7 @@ namespace stardew_access.ScreenReader
         //
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern void init_speaker();
-        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
         private static extern void speak(string text);
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern void set_voice(Int32 index);
@@ -36,7 +36,7 @@ namespace stardew_access.ScreenReader
         private static extern void set_language(Int32 index);
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern UInt32 available_languages_count();
-        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
         private static extern void get_voice_name(UInt32 idx, String pszOut);
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern void set_volume(Single volume);
@@ -60,7 +60,7 @@ namespace stardew_access.ScreenReader
         //
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern IntPtr make_speaker();
-        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
         private static extern void speak_with(IntPtr speaker, String text);
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern void set_voice_with(IntPtr speaker, Int32 index);
@@ -100,7 +100,7 @@ namespace stardew_access.ScreenReader
         private static extern void start_listening(IntPtr listener);
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern void stop_listening(IntPtr listener);
-        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
         private static extern void add_command(IntPtr listener, String command);
         [DllImport("libspeak", CallingConvention = CallingConvention.Cdecl)]
         private static extern void cleanup_listener(IntPtr listener);
