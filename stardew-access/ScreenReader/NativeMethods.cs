03fd81565096dc3aea0ea8121af76c231d9b20dd
Katie Durden <1766632+ParadoxiKat@users.noreply.github.com>
2023-06-10 13:29:15 -0700

    fix CA1401: P/Invoke method 'SetDllDirectory' should not be visible

--- stardew-access/ScreenReader/NativeMethods.cs
+++ stardew-access/ScreenReader/NativeMethods.cs
@@ -5,6 +5,6 @@ namespace stardew_access.ScreenReader
     public static class NativeMethods
     {
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern bool SetDllDirectory(string lpPathName);
+        internal static extern bool SetDllDirectory(string lpPathName);
     }
 }
