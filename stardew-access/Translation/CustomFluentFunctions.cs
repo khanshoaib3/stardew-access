e41fc816f92c4f5c0bcd830bb4feba1c906e32c9
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2023-06-14 15:44:14 +0530

    Added SIGNOFNUMBER function and updated the health and stamine recovery entry

--- stardew-access/Translation/CustomFluentFunctions.cs
+++ stardew-access/Translation/CustomFluentFunctions.cs
@@ -16,8 +16,8 @@ namespace stardew_access
 
         internal IEnumerable<(IManifest mod, string name, FluentFunction function)> GetAll()
         {
-            yield return (ModManifest, "TEST", EE);
             yield return (ModManifest, "EMPTYSTRING", EmptyString);
+            yield return (ModManifest, "SIGNOFNUMBER", SignOfNumber);
         }
 
         /// <summary>
@@ -42,14 +42,46 @@ namespace stardew_access
             return FluentApi.CreateStringValue("");
         }
 
-        private IFluentFunctionValue EE(
+        /// <summary>
+        /// Determines the sign of a number and returns the corresponding value.
+        /// </summary>
+        /// <param name="positionalArguments">(At 0th index) The number to check for the sign</param>
+        /// <returns><para>
+        ///     A string value representing the sign of the number.
+        ///     Possible return values:
+        ///     - "negative" if the number is negative
+        ///     - "positive" if the number is positive
+        ///     - "zero" if the number is zero
+        ///     - "unknown" if no positional arguments are provided or if the provided argument is not a valid number
+        /// 
+        ///     Note: Returning an integer value instead of a string value would prevent proper distinction in the Fluent file (.ftl) (only for custom functions).
+        /// </para></returns>
+        private IFluentFunctionValue SignOfNumber(
             IGameLocale locale,
             IManifest mod,
             IReadOnlyList<IFluentFunctionValue> positionalArguments,
             IReadOnlyDictionary<string, IFluentFunctionValue> namedArguments
         )
         {
-            return FluentApi.CreateIntValue(-1);
+            if (positionalArguments.Count <= 0)
+                return FluentApi.CreateStringValue("unknown");
+
+            var value = positionalArguments[0].AsFloatOrNull();
+            if (value != null)
+            {
+                if (value > 0)
+                {
+                    return FluentApi.CreateStringValue("positive");
+                }
+                if (value < 0)
+                {
+                    return FluentApi.CreateStringValue("negative");
+                }
+
+                return FluentApi.CreateStringValue("zero");
+            }
+
+            return FluentApi.CreateStringValue("unknown");
         }
     }
 }
