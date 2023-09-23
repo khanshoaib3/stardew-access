ff1eacf94d2d95bdc1f17e3985ef7953f8928629
Katie Tudela <1766632+ParadoxiKat@users.noreply.github.com>
2023-09-23 11:38:22 -0700

    feat: Extend OverlayedDictionary with Layer-Specific Operations and Metadata
    - Implement ability to associate names with layers for easier management and identification.
    - Add overloaded versions of layer management methods and IDictionary interface methods, enabling operations on specific layers identified by name.
    - Enhance CustomDictBase and OverlayedDictionary to respect the `IsReadOnly` flag for individual layers.
    - Introduce a new function to fetch all values under a given key across all layers.
    
    These changes provide more flexible and granular control over the layers in OverlayedDictionary, making it more robust and usable in different scenarios.

--- stardew-access/Utils/OverlayedDictionary.cs
+++ stardew-access/Utils/OverlayedDictionary.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 
 namespace stardew_access.Utils
 {
+    #region CustomDictBase
     public abstract class CustomDictBase<TKey, TValue> : IDictionary<TKey, TValue> where TKey : notnull
     {
         // Abstract methods that subclasses must implement
@@ -64,6 +65,7 @@ namespace stardew_access.Utils
 
         virtual public void Clear()
         {
+            if (IsReadOnly) throw new InvalidOperationException("Dictionary is read-only.");
             foreach (TKey key in Keys)
             {
                 Remove(key);
@@ -119,44 +121,143 @@ namespace stardew_access.Utils
             return TryAdd(kvp.Key, kvp.Value);
         }
     }
+    #endregion
 
+    #region OverlayedDictionary
     public class OverlayedDictionary<TKey, TValue> : CustomDictBase<TKey, TValue> where TKey : notnull
     {
-        private readonly List<IDictionary<TKey, TValue>> internalDictionaries;
+        private readonly List<IDictionary<TKey, TValue>> layers;
+        private readonly Dictionary<string, IDictionary<TKey, TValue>> layersByName;
 
+        #region Constructors
         // Constructor 1: Takes an existing list of dictionaries
-        public OverlayedDictionary(List<IDictionary<TKey, TValue>> dictionaries)
+        public OverlayedDictionary(List<IDictionary<TKey, TValue>> dictionaries, List<string?>? layerNames = null)
         {
-            this.internalDictionaries = dictionaries;
-        }
+            // Initialize layers
+            this.layers = dictionaries;
+            this.layersByName = new();
 
-        // Constructor 2: Takes a single dictionary, adds it to a new list, and calls Constructor 1
-        public OverlayedDictionary(IDictionary<TKey, TValue> singleDict) : this(new List<IDictionary<TKey, TValue>> { singleDict }) { }
-
-        // Constructor 3: Creates an empty dictionary, then calls Constructor 2, which in turn calls Constructor 1
-        public OverlayedDictionary() : this(new Dictionary<TKey, TValue>()) { }
-
-        public void AddLayer(IDictionary<TKey, TValue> newLayer)
-        {
-            internalDictionaries.Add(newLayer);
-        }
-
-        public void RemoveLayer()
-        {
-            if (internalDictionaries.Count > 0)
+            // If layer names are provided, ensure the counts match
+            if (layerNames != null)
             {
-                internalDictionaries.RemoveAt(internalDictionaries.Count - 1);
+                if (layerNames.Count != dictionaries.Count)
+                {
+                    throw new ArgumentException("The number of layer names must match the number of dictionaries.");
+                }
+
+                // Populate the named layers
+                for (int i = 0; i < layerNames.Count; i++)
+                {
+                    string? name = layerNames[i];
+                    if (name != null)
+                    {
+                        layersByName[name] = dictionaries[i];
+                    }
+                }
             }
         }
 
-        public IDictionary<TKey, TValue> GetLayer(int index)
+        // Constructor 2: Takes a single dictionary, adds it to a new list, and calls Constructor 1
+        public OverlayedDictionary(IDictionary<TKey, TValue> singleLayer, string? singleLayerName = null) : this(new List<IDictionary<TKey, TValue>> { singleLayer }, singleLayerName == null ? null : new List<string?> { singleLayerName }) { }
+
+        // Constructor 3: Creates an empty dictionary, then calls Constructor 2, which in turn calls Constructor 1
+        public OverlayedDictionary(string? singleLayerName = null) : this(new Dictionary<TKey, TValue>(), singleLayerName) { }
+        #endregion
+
+        #region Layer Management
+        public void AddLayer(IDictionary<TKey, TValue> newLayer, string? layerName = null)
         {
-            return internalDictionaries[index];
+            layers.Add(newLayer);
+            if (layerName != null)
+                layersByName.Add(layerName, newLayer);
         }
 
         public bool ContainsLayer(IDictionary<TKey, TValue> layer)
         {
-            return internalDictionaries.Contains(layer);
+            return layers.Contains(layer);
+        }
+
+        public IDictionary<TKey, TValue>? GetLayer(int index)
+        {
+            if (index >= 0 && index < layers.Count)
+            {
+                return layers[index];
+            }
+            return null;
+        }
+
+        public IDictionary<TKey, TValue>? GetLayer(string layerName)
+        {
+            layersByName.TryGetValue(layerName, out IDictionary<TKey, TValue>? layer);
+            return layer;
+        }
+
+        public string? GetLayerName(IDictionary<TKey, TValue> layer)
+        {
+            return layersByName.FirstOrDefault(x => x.Value.Equals(layer)).Key;
+        }
+
+        private IDictionary<TKey, TValue> GetReadableLayer(string layerName)
+        {
+            // Fetch the specific layer by its name
+            if (layersByName.TryGetValue(layerName, out IDictionary<TKey, TValue>? layer) && layer != null)
+            {
+                // Check if the layer exists and is not read-only
+                if (layer.IsReadOnly)
+                {
+                    throw new InvalidOperationException($"The layer '{layerName}' is read-only.");
+                }
+                return layer;
+            }
+            else
+            {
+                throw new ArgumentException($"No layer exists with the name '{layerName}'.");
+            }
+        }
+
+        public void RemoveLayer(IDictionary<TKey, TValue>? layerToRemove = null)
+        {
+            if (layerToRemove == null && layers.Count > 0)
+            {
+                layerToRemove = layers[^1];  // Get the last layer
+            }
+            
+            if (layerToRemove != null)
+            {
+                layers.Remove(layerToRemove);
+                
+                // Remove from layersByName if it exists
+                string keyToRemove = layersByName.FirstOrDefault(x => x.Value.Equals(layerToRemove)).Key;
+                if (!string.IsNullOrEmpty(keyToRemove))
+                {
+                    layersByName.Remove(keyToRemove);
+                }
+            }
+        }
+
+        public void RemoveLayer(int index)
+        {
+            if (index >= 0 && index < layers.Count)
+            {
+                IDictionary<TKey, TValue> layerToRemove = layers[index];
+                RemoveLayer(layerToRemove);
+            }
+            else
+            {
+                Log.Verbose($"No such layer exists at index {index}.", true);
+            }
+        }
+
+        public void RemoveLayer(string layerName)
+        {
+            if (layersByName.TryGetValue(layerName, out IDictionary<TKey, TValue>? layerToRemove))
+            {
+                RemoveLayer(layerToRemove!);
+            }
+            else
+            {
+                Log.Verbose($"No such layer exists by key name {layerName}.", true);
+            }
         }
 
         public Dictionary<TKey, TValue> Snapshot
@@ -166,9 +267,9 @@ namespace stardew_access.Utils
                 Dictionary<TKey, TValue> snapshot = new();
 
                 // Loop over the dictionaries in reverse order (top layer first)
-                for (int i = internalDictionaries.Count - 1; i >= 0; i--)
+                for (int i = layers.Count - 1; i >= 0; i--)
                 {
-                    var layer = internalDictionaries[i];
+                    var layer = layers[i];
                     foreach (var kvp in layer)
                     {
                         snapshot.TryAdd(kvp.Key, kvp.Value);  // Using Dictionary's built-in TryAdd method
@@ -178,7 +279,9 @@ namespace stardew_access.Utils
                 return snapshot;
             }
         }
+        #endregion
 
+        #region Dictionary Interface
         public static explicit operator Dictionary<TKey, TValue>(OverlayedDictionary<TKey, TValue> overlayed)
         {
             return overlayed.Snapshot; // or whatever you end up naming the property
@@ -186,18 +289,28 @@ namespace stardew_access.Utils
 
         public override void Add(TKey key, TValue value)
         {
-            internalDictionaries[^1].Add(key, value);  // Add to the top-most layer
+            if (IsReadOnly) throw new InvalidOperationException("Dictionary is read-only.");
+            layers[^1].Add(key, value);  // Add to the top-most layer
         }
+
+        public void Add(TKey key, TValue value, string layerName) => GetReadableLayer(layerName).Add(key, value);
+
+        public void Add(KeyValuePair<TKey, TValue> item, string layerName) => GetReadableLayer(layerName).Add(item);
 
         public override void Clear()
         {
-            internalDictionaries.Clear();
-            internalDictionaries.Add(new Dictionary<TKey, TValue>());  // Add an empty dictionary as the new bottom layer
+            if (IsReadOnly) throw new InvalidOperationException("Dictionary is read-only.");
+            layers.Clear();
+            layers.Add(new Dictionary<TKey, TValue>());  // Add an empty dictionary as the new bottom layer
         }
+
+        public void Clear(string layerName) => GetReadableLayer(layerName).Clear();
+        
+        public bool Contains(KeyValuePair<TKey, TValue> item, string layerName) => GetReadableLayer(layerName).Contains(item);
 
         public override bool ContainsKey(TKey key)
         {
-            foreach (IDictionary<TKey, TValue> dict in internalDictionaries)
+            foreach (IDictionary<TKey, TValue> dict in layers)
             {
                 if (dict.ContainsKey(key))
                 {
@@ -207,12 +320,30 @@ namespace stardew_access.Utils
             return false;
         }
 
+        public bool ContainsKey(TKey key, string layerName) => GetReadableLayer(layerName).ContainsKey(key);
+
+        public List<TValue> GetAllValuesForKey(TKey key)
+        {
+            List<TValue> values = new();
+            
+            // Iterate backwards over the layers to preserve the overlay order
+            for (int i = layers.Count - 1; i >= 0; i--)
+            {
+                if (layers[i].TryGetValue(key, out TValue? value))
+                {
+                    values.Add(value!);
+                }
+            }
+            
+            return values;
+        }
+
         public override IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             HashSet<TKey> seenKeys = new();
-            for (int i = internalDictionaries.Count - 1; i >= 0; i--)
+            for (int i = layers.Count - 1; i >= 0; i--)
             {
-                IDictionary<TKey, TValue> dict = internalDictionaries[i];
+                IDictionary<TKey, TValue> dict = layers[i];
                 foreach (KeyValuePair<TKey, TValue> kvp in dict)
                 {
                     // Skip this key if we've already seen it in a higher layer
@@ -228,18 +359,26 @@ namespace stardew_access.Utils
             }
         }
 
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator(string layerName) => GetReadableLayer(layerName).GetEnumerator();
+
         public override bool Remove(TKey key)
         {
-            for (int i = internalDictionaries.Count - 1; i >= 0; i--)
+            if (IsReadOnly) throw new InvalidOperationException("Dictionary is read-only.");
+            for (int i = layers.Count - 1; i >= 0; i--)
             {
-                if (internalDictionaries[i].ContainsKey(key))
+                if (layers[i].ContainsKey(key))
                 {
-                    internalDictionaries[i].Remove(key);
+                    layers[i].Remove(key);
                     return true;  // Remove only the top-most occurrence and break
                 }
             }
             return false;
         }
+
+        public bool Remove(TKey key, string layerName) => GetReadableLayer(layerName).Remove(key);
+
+
+        public bool Remove(KeyValuePair<TKey, TValue> item, string layerName) => GetReadableLayer(layerName).Remove(item);
 
         public bool Remove(TKey key, bool all)
         {
@@ -250,11 +389,13 @@ namespace stardew_access.Utils
             else
             {
                 bool removed = false;
-                for (int i = internalDictionaries.Count - 1; i >= 0; i--)
+                for (int i = layers.Count - 1; i >= 0; i--)
                 {
-                    if (internalDictionaries[i].ContainsKey(key))
+                    if (layers[i].ContainsKey(key))
                     {
-                        internalDictionaries[i].Remove(key);
+                        if (IsReadOnly) Log.Warn($"Layer at index {i} is read-only; cannot remove key {key}.");
+
+                        layers[i].Remove(key);
                         removed = true;
                     }
                 }
@@ -271,9 +412,11 @@ namespace stardew_access.Utils
             return false;
         }
 
+        public bool TryAdd(TKey key, TValue value, string layerName) => GetReadableLayer(layerName).TryAdd(key, value);
+
         public override bool TryGetValue(TKey key, out TValue value)
         {
-            foreach (IDictionary<TKey, TValue> dict in internalDictionaries)
+            foreach (IDictionary<TKey, TValue> dict in layers)
             {
                 // possible null value is expected here
                 #pragma warning disable CS8601 // Possible null reference assignment.
@@ -283,8 +426,12 @@ namespace stardew_access.Utils
                 }
             }
             value = default;
-            #pragma warning restore CS8601 // Possible null reference assignment.
             return false;
         }
+
+        public bool TryGetValue(TKey key, out TValue value, string layerName) => GetReadableLayer(layerName).TryGetValue(key, out value);
+        #pragma warning restore CS8601 // Possible null reference assignment.
+        #endregion
     }
+    #endregion
 }
