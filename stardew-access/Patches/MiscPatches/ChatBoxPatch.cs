f937b6f81ac4c25ddcb44391e8cef6bd0741c190
Mohammad Shoaib Khan <khanshoaibpers@outlook.com>
2024-02-06 15:27:15 +0530

    :sparkles: Improve Chat Box Message Reading #197
    PageUp and PageDown now correctly cycle through the chat messages.
    The index also resets when the chatbox is closed.

--- stardew-access/Patches/MiscPatches/ChatBoxPatch.cs
+++ stardew-access/Patches/MiscPatches/ChatBoxPatch.cs
@@ -1,80 +1,88 @@
 ﻿using HarmonyLib;
-using Microsoft.Xna.Framework;
 using StardewValley.Menus;
 
-namespace stardew_access.Patches
+namespace stardew_access.Patches;
+
+internal class ChatBoxPatch : IPatch
 {
-    internal class ChatBoxPatch : IPatch
+    private static int currentChatMessageIndex = 0;
+    private static string prevTextBoxMessage = "";
+    private static bool isChatRunning = false;
+
+    public void Apply(Harmony harmony)
     {
-        private static int currentChatMessageIndex = 0;
-        private static bool isChatRunning = false;
+        harmony.Patch(
+           original: AccessTools.DeclaredMethod(typeof(ChatBox), "update"),
+           postfix: new HarmonyMethod(typeof(ChatBoxPatch), nameof(ChatBoxPatch.UpdatePatch))
+        );
+    }
 
-        public void Apply(Harmony harmony)
+    private static void UpdatePatch(ChatBox __instance, List<ChatMessage> ___messages)
+    {
+        try
         {
-            harmony.Patch(
-                    original: AccessTools.Method(typeof(ChatBox), nameof(ChatBox.update), new Type[] { typeof(GameTime) }),
-                    postfix: new HarmonyMethod(typeof(ChatBoxPatch), nameof(ChatBoxPatch.UpdatePatch))
-                );
-        }
-
-        private static void UpdatePatch(ChatBox __instance, List<ChatMessage> ___messages)
-        {
-            try
+            if (__instance.chatBox.Selected)
             {
-                string toSpeak = " ";
+                bool isPrevButtonPressed = MainClass.Config.ChatMenuNextKey.JustPressed();
+                bool isNextButtonPressed = MainClass.Config.ChatMenuPreviousKey.JustPressed();
 
-                if (__instance.chatBox.Selected)
+                if (isNextButtonPressed && !isChatRunning)
                 {
-                    bool isPrevButtonPressed = MainClass.Config.ChatMenuNextKey.JustPressed();
-                    bool isNextButtonPressed = MainClass.Config.ChatMenuPreviousKey.JustPressed();
-
-                    if (___messages.Count <= 0) return;
-
-                    #region To narrate previous and next chat messages
-                    if (isNextButtonPressed && !isChatRunning)
-                    {
-                        isChatRunning = true;
-                        CycleThroughChatMessages(true, ___messages);
-                        Task.Delay(200).ContinueWith(_ => { isChatRunning = false; });
-                    }
-                    else if (isPrevButtonPressed && !isChatRunning)
-                    {
-                        isChatRunning = true;
-                        CycleThroughChatMessages(false, ___messages);
-                        Task.Delay(200).ContinueWith(_ => { isChatRunning = false; });
-                    }
-                    #endregion
+                    isChatRunning = true;
+                    CycleThroughChatMessages(false, ___messages);
+                    Task.Delay(200).ContinueWith(_ => { isChatRunning = false; });
                 }
-                else if (___messages.Count > 0)
+                else if (isPrevButtonPressed && !isChatRunning)
                 {
-                    #region To narrate latest chat message
-                    ___messages[^1].message.ForEach(message =>
+                    isChatRunning = true;
+                    CycleThroughChatMessages(true, ___messages);
+                    Task.Delay(200).ContinueWith(_ => { isChatRunning = false; });
+                }
+
+                Log.Info($"{__instance.chatBox.Text}");
+                if (prevTextBoxMessage != __instance.chatBox.Text)
+                {
+                    prevTextBoxMessage = __instance.chatBox.Text;
+                    MainClass.ScreenReader.Say(prevTextBoxMessage, true);
+                }
+            }
+            else
+            {
+                currentChatMessageIndex = 0;
+                if (___messages.Count > 0)
+                {
+                    string toSpeak = GetMessage(___messages[^1]);
+                    if (!string.IsNullOrWhiteSpace(toSpeak))
                     {
-                        toSpeak += $"{message.message}, ";
-                    });
-                    if (toSpeak != " ")
                         MainClass.ScreenReader.SayWithChatChecker(toSpeak, false);
-                    #endregion
+                    }
                 }
             }
-            catch (Exception e)
-            {
-                Log.Error($"An error occurred in chat box patch:\n{e.Message}\n{e.StackTrace}");
-            }
         }
-
-        private static void CycleThroughChatMessages(bool increase, List<ChatMessage> ___messages)
+        catch (Exception e)
         {
-            string toSpeak = " ";
-
-            currentChatMessageIndex = (increase) ? (Math.Min(currentChatMessageIndex + 1, ___messages.Count - 1)) : (currentChatMessageIndex = Math.Max(currentChatMessageIndex - 1, 0));
-
-            ___messages[currentChatMessageIndex].message.ForEach(message =>
-            {
-                toSpeak += $"{message.message}, ";
-            });
-
-            MainClass.ScreenReader.Say(toSpeak, true);
+            Log.Error($"An error occurred in chat box patch:\n{e.Message}\n{e.StackTrace}");
         }
+    }
+
+    private static void CycleThroughChatMessages(bool prev, List<ChatMessage> ___messages)
+    {
+        if (___messages.Count <= 0) return;
+
+        currentChatMessageIndex = prev
+            ? Math.Min(currentChatMessageIndex + 1, ___messages.Count)
+            : Math.Max(currentChatMessageIndex - 1, 1);
+
+        MainClass.ScreenReader.Say(GetMessage(___messages[^currentChatMessageIndex]), true);
+    }
+
+    private static string GetMessage(ChatMessage chatMessage)
+    {
+        if (chatMessage.message.Count <= 1)
+            return chatMessage.message[0].message;
+
+        string toReturn = "";
+        chatMessage.message.ForEach(chatSnippet => toReturn += $"{chatSnippet.message}, ");
+        return toReturn;
     }
 }
