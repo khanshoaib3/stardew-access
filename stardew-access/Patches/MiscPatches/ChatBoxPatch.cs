c286db462ab3228ff174ca9cade407a3d25c8d7d
Mohammad Shoaib Khan <khanshoaibpers@outlook.com>
2024-02-10 23:21:32 +0530

    ✨ Add minecraft access like prev message reading
    - We can use LeftAlt + num keys (1-9) to speak previous chat messages
    
    *Need to suppress keys when left alt is pressed*

--- stardew-access/Patches/MiscPatches/ChatBoxPatch.cs
+++ stardew-access/Patches/MiscPatches/ChatBoxPatch.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using Microsoft.Xna.Framework.Input;
+using StardewValley;
 using StardewValley.Menus;
 
 namespace stardew_access.Patches;
@@ -23,8 +25,41 @@ internal class ChatBoxPatch : IPatch
         {
             if (__instance.chatBox.Selected)
             {
+                bool isLeftAltPressed = Game1.input.GetKeyboardState().IsKeyDown(Keys.LeftAlt);
+
                 bool isPrevButtonPressed = MainClass.Config.ChatMenuNextKey.JustPressed();
                 bool isNextButtonPressed = MainClass.Config.ChatMenuPreviousKey.JustPressed();
+
+                if (isLeftAltPressed && !isChatRunning)
+                {
+                    int pressedNumKey = -1;
+                    foreach (var key in Game1.input.GetKeyboardState().GetPressedKeys())
+                    {
+                        pressedNumKey = key switch
+                        {
+                            Keys.NumPad1 or Keys.D1 => 1,
+                            Keys.NumPad2 or Keys.D2 => 2,
+                            Keys.NumPad3 or Keys.D3 => 3,
+                            Keys.NumPad4 or Keys.D4 => 4,
+                            Keys.NumPad5 or Keys.D5 => 5,
+                            Keys.NumPad6 or Keys.D6 => 6,
+                            Keys.NumPad7 or Keys.D7 => 7,
+                            Keys.NumPad8 or Keys.D8 => 8,
+                            Keys.NumPad9 or Keys.D9 => 9,
+                            _ => -1,
+                        };
+
+                        if (pressedNumKey != -1) break;
+                    }
+
+                    if (pressedNumKey != -1 && ___messages.Count >= pressedNumKey)
+                    {
+                        isChatRunning = true;
+                        MainClass.ScreenReader.Say(GetMessage(___messages[^pressedNumKey]), true);
+                        Task.Delay(200).ContinueWith(_ => { isChatRunning = false; });
+                    }
+                    return;
+                }
 
                 if (isNextButtonPressed && !isChatRunning)
                 {
@@ -39,7 +74,7 @@ internal class ChatBoxPatch : IPatch
                     Task.Delay(200).ContinueWith(_ => { isChatRunning = false; });
                 }
 
-                Log.Info($"{__instance.chatBox.Text}");
+                // Log.Info($"{__instance.chatBox.Text}");
                 if (prevTextBoxMessage != __instance.chatBox.Text)
                 {
                     prevTextBoxMessage = __instance.chatBox.Text;
