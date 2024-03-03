9ba76171ca8d0a62513e4aa2f65df9897ec924aa
Katie Tudela <1766632+ParadoxiKat@users.noreply.github.com>
2024-03-02 23:50:45 -0800

    Update `ObjectUtils.GetObjectById` To Remove "(O)" Prefix From Ids

--- stardew-access/Utils/ObjectUtils.cs
+++ stardew-access/Utils/ObjectUtils.cs
@@ -7,6 +7,8 @@ public static class ObjectUtils
 {
     public static ObjectData GetObjectById(string objectId)
     {
+        if (objectId.StartsWith("(O)"))
+            objectId = objectId.Substring(3); // "(O)" is 3 characters long, so start after it
         if (!Game1.objectData.TryGetValue(objectId, out ObjectData? objectInfo) || objectInfo is null)
             throw new ArgumentException($"Object ID {objectId} does not exist.");
 
