34883efbd94053953b6518c32cd3812b8d19850e
Mohammad Shoaib Khan <khanshoaibpers@outlook.com>
2024-03-17 14:51:48 +0530

    ✨ Speak category wise profit break down
    The game speaks the breakdown as the page opens, we can also use the PrimaryInfoKey to repeat the profit list.
    The menu also now speaks the date.

--- stardew-access/Patches/OtherMenuPatches/ShippingMenuPatch.cs
+++ stardew-access/Patches/OtherMenuPatches/ShippingMenuPatch.cs
@@ -1,35 +1,43 @@
 using HarmonyLib;
-using Microsoft.Xna.Framework.Graphics;
+using stardew_access.Translation;
+using stardew_access.Utils;
 using StardewValley;
 using StardewValley.Menus;
 
-namespace stardew_access.Patches
+namespace stardew_access.Patches;
+
+internal class ShippingMenuPatch : IPatch
 {
-    // TODO Speak per category money distribution info
-    internal class ShippingMenuPatch : IPatch
+    private static int previousTab = -1;
+
+    public void Apply(Harmony harmony)
     {
-        public void Apply(Harmony harmony)
-        {
-            harmony.Patch(
-                original: AccessTools.Method(typeof(ShippingMenu), nameof(ShippingMenu.draw), new Type[] { typeof(SpriteBatch) }),
-                postfix: new HarmonyMethod(typeof(ShippingMenuPatch), nameof(ShippingMenuPatch.DrawPatch))
-            );
-        }
+        harmony.Patch(
+            original: AccessTools.DeclaredMethod(typeof(ShippingMenu), "draw"),
+            postfix: new HarmonyMethod(typeof(ShippingMenuPatch), nameof(ShippingMenuPatch.DrawPatch))
+        );
+    }
 
-        private static void DrawPatch(ShippingMenu __instance, List<int> ___categoryTotals)
+    private static void DrawPatch(ShippingMenu __instance, List<int> ___categoryTotals, List<List<Item>> ___categoryItems, Dictionary<Item, int> ___itemValues, Dictionary<Item, int> ___singleItemValues, bool ___outro, bool ___newDayPlaque)
+    {
+        try
         {
-            try
+            int x = Game1.getMouseX(true), y = Game1.getMouseY(true); // Mouse x and y position
+
+            MouseUtils.SimulateMouseClicks((x, y) => __instance.receiveLeftClick(x, y),
+                    (x, y) => __instance.receiveRightClick(x, y));
+
+            if (___outro)
             {
-                if (__instance.currentPage != -1)
-                    return;
+                if (___newDayPlaque)
+                    MainClass.ScreenReader.SayWithMenuChecker(Utility.getDateString(), true);
+                previousTab = -1;
+                return;
+            }
 
-                int x = Game1.getMouseX(true), y = Game1.getMouseY(true); // Mouse x and y position
-
-                // Perform Left Click
-                if (MainClass.Config.LeftClickMainKey.JustPressed() || MainClass.Config.LeftClickAlternateKey.JustPressed())
-                {
-                    Game1.activeClickableMenu.receiveLeftClick(x, y);
-                }
+            if (__instance.currentPage == -1)
+            {
+                previousTab = -1;
 
                 int total = ___categoryTotals[5];
                 string toSpeak;
@@ -60,10 +68,47 @@ namespace stardew_access.Patches
                     return;
                 }
             }
-            catch (Exception e)
+            else
             {
-                Log.Error($"An error occurred in shipping menu patch:\n{e.Message}\n{e.StackTrace}");
+                // Speak category wise profit break down
+                List<string> displayedItems = new();
+                for (int i = __instance.currentTab * __instance.itemsPerCategoryPage; i < __instance.currentTab * __instance.itemsPerCategoryPage + __instance.itemsPerCategoryPage; i++)
+                {
+                    if (___categoryItems[__instance.currentPage].Count <= i)
+                        continue;
+                    Item item = ___categoryItems[__instance.currentPage][i];
+                    string singleVal = Game1.content.LoadString("Strings\\StringsFromCSFiles:LoadGameMenu.cs.11020", ___singleItemValues[item]);
+                    string totalVal = Game1.content.LoadString("Strings\\StringsFromCSFiles:LoadGameMenu.cs.11020", ___itemValues[item]);
+                    displayedItems.Add($"{InventoryUtils.GetItemDetails(item)} x{singleVal} = {totalVal}");
+                }
+
+                if (MainClass.Config.PrimaryInfoKey.JustPressed())
+                {
+                    MainClass.ScreenReader.Say(string.Join(", ", displayedItems), true);
+                    return;
+                }
+
+                string displayedItemsString = __instance.currentTab != previousTab ? string.Join(", ", displayedItems) + "\n" : "";
+                string hoveredButton = "";
+
+                if (__instance.backButton is { visible: true } && __instance.backButton.containsPoint(x, y))
+                {
+                    hoveredButton = Translator.Instance.Translate(__instance.currentTab is not 0 ? "common-ui-previous_page_button" : "common-ui-back_button", translationCategory: TranslationCategory.Menu);
+                }
+                else if (__instance.forwardButton is { visible: true } && __instance.showForwardButton() && __instance.forwardButton.containsPoint(x, y))
+                {
+                    hoveredButton = Translator.Instance.Translate("common-ui-next_page_button", translationCategory: TranslationCategory.Menu);
+                }
+
+                if (MainClass.ScreenReader.SayWithMenuChecker(displayedItemsString + hoveredButton, true, customQuery: hoveredButton))
+                {
+                    previousTab = __instance.currentTab;
+                }
             }
+        }
+        catch (Exception e)
+        {
+            Log.Error($"An error occurred in shipping menu patch:\n{e.Message}\n{e.StackTrace}");
         }
     }
 }
